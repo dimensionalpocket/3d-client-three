7e02d1901a20cd87f12b9b0466433a9d95284570
pauldps <github@pauloddr.com>
2022-09-18 20:48:00 -0400

    ThreeSound, initial implementation

---
+++ src/ThreeSound.js
@@ -0,0 +1,82 @@
+import { Audio, PositionalAudio } from 'three'
+import { Sound } from './core/Sound.js'
+
+export class ThreeSound extends Sound {
+  /**
+   * @param {SoundOptions} options
+   */
+  constructor (options) {
+    super(options)
+
+    /**
+     * @type {THREE.Audio|THREE.PositionalAudio|undefined}
+     */
+    this.renderable = undefined
+  }
+
+  /**
+   * @param {ThreeClient} client
+   */
+  install (client) {
+    const audio = client.audio
+
+    /**
+     * @type {?THREE.AudioListener}
+     */
+    let output = null
+    let outputVolume = 1.0
+
+    if (this.type === 'effect') {
+      output = audio.effectsOutput
+      outputVolume = audio.effectsVolume
+    } else if (this.type === 'voice') {
+      output = audio.voiceOutput
+      outputVolume = audio.voiceVolume
+    } else if (this.type === 'music') {
+      output = audio.musicOutput
+      outputVolume = audio.musicVolume
+    }
+
+    if (output == null) return
+
+    return this.soundFile.install().then(soundFile => {
+      /**
+       * @type {THREE.Audio|THREE.PositionalAudio}
+       */
+      let sound
+
+      if (this.positional === true) {
+        // @ts-ignore - TS barfing again, missing the guard clause
+        sound = new PositionalAudio(output)
+        sound.setRefDistance(this.distance)
+      } else {
+        // @ts-ignore - conflict with DOM lib
+        sound = new Audio(output)
+      }
+
+      sound.offset = this.offset
+      sound.duration = this.duration
+
+      sound.setVolume(this.volume * outputVolume)
+
+      const buffer = soundFile.buffer
+
+      if (buffer == null) {
+        throw new Error(`ThreeSound#install: failed to fetch buffer from SoundFile<${soundFile.id}>`)
+      }
+
+      sound.setBuffer(buffer)
+
+      this.renderable = sound
+
+      return this
+    })
+  }
+
+  play () {
+    const renderable = this.renderable
+    if (renderable == null) return false
+
+    renderable.play()
+  }
+}
